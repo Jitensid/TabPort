b20ddad8027e70e5966a5465af218c91878bac69
Luiz Paiva <luizgustavodematos@gmail.com>
2022-10-17 22:21:12 -0300

    Fix Copy Button and FileFormat tests

--- src/__tests__/Utils/FileFormat.test.js
+++ src/__tests__/Utils/FileFormat.test.js
@@ -1,10 +1,13 @@
-import { determinUrlType, fileFormats } from "../../utils/FileFormat";
+import { determinUrlType, fileFormats } from '../../utils/FileFormat';
 
-describe("FileFormat Util", () => {
-    test("determinUrlType returns correct type", () => {
-        const x = determinUrlType(fileFormats.json);
-        const y = determinUrlType(fileFormats.txt);
-        expect(x).toBe("application/json");
-        expect(y).toBe("text/plain");
-    });
+describe('FileFormat Util', () => {
+	test('determinUrlType returns correct type', () => {
+		const json = determinUrlType(fileFormats.json);
+		const txt = determinUrlType(fileFormats.txt);
+		const other = determinUrlType('other');
+
+		expect(json).toBe('application/json');
+		expect(txt).toBe('text/plain');
+		expect(other).toBe('application/json');
+	});
 });
