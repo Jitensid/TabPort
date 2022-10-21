5f1b1f02c013204770f894bf10ee40d9ac229407
Saurav jha <jhasaurav215@gmail.com>
2022-10-21 12:09:13 +0530

    added more tests in UploadTabsList.test.js

--- src/__tests__/components/UploadTabsList/UploadTabsList.test.js
+++ src/__tests__/components/UploadTabsList/UploadTabsList.test.js
@@ -1,16 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import UploadTabsList from '../../../components/UploadTabsList/UploadTabsList';
 
+const uploadedTabsToOpen = [
+	{ url: 'url1', title: 'title1' },
+	{ url: 'url2', title: 'title2' },
+]
+
 describe('UploadTabsList component working properly', () => {
     let openAllTabsCheckBoxElement;
-    let openTabsButtonElementOfUploadTabsListComponent;
+    let openTabsButtonElement;
 
     beforeEach(() => {
         render(
 			<UploadTabsList
-                uploadedTabsToOpen={["google.com"]}				
+                uploadedTabsToOpen={uploadedTabsToOpen}				
 			/>
 		);
+        
+        // Open All Tabs Checkbox
+        openAllTabsCheckBoxElement = screen.getByRole('checkbox');
+        
+        // Open Tabs Button
+        openTabsButtonElement =
+            screen.getByRole('button', {
+                name: 'Open Tabs',
+            });
     });
     
 	afterEach(() => {
@@ -18,26 +32,63 @@ describe('UploadTabsList component working properly', () => {
 	});
 
     test('Open Tabs Button and Open All Tabs is rendered properly', () => {
-        // Open All Tabs Checkbox
-        openAllTabsCheckBoxElement = screen.getByRole('checkbox');
-
-        // Open Tabs Button
-        openTabsButtonElementOfUploadTabsListComponent =
-            screen.getByRole('button', {
-                name: 'Open Tabs',
-            });
 		// Checking if the checkbox and open tabs button is present or not
 		expect(openAllTabsCheckBoxElement).toBeInTheDocument();
 		expect(
-			openTabsButtonElementOfUploadTabsListComponent
+			openTabsButtonElement
 		).toBeInTheDocument();
     })
 
 	test('OpenAllTabsCheckbox is already checked by default', () => {
-        // Open All Tabs Checkbox
-        openAllTabsCheckBoxElement = screen.getByRole('checkbox');
-
 		// test if the downloadAllTabsCheckBox is by default checked
 		expect(openAllTabsCheckBoxElement).toHaveProperty('checked', true);
+	});
+
+
+	// uncheck the downloadAllTabsCheckbox for this test suite
+	describe('downloadAllTabsCheckbox is unchecked', () => {
+		beforeEach(() => {
+			// uncheck the downloadAllTabsCheckbox
+			fireEvent.click(openAllTabsCheckBoxElement);
+		});
+
+		afterEach(() => {
+			jest.restoreAllMocks();
+		});
+
+		test('List of tabs is displayed properly', () => {
+
+			expect(openAllTabsCheckBoxElement).toHaveProperty(
+				'checked',
+				false
+			);
+
+			expect(
+				screen.getByText(uploadedTabsToOpen[0]['title'])
+			).toBeInTheDocument();
+		});
+
+		test('uncheck single tab from the tabs list', () => {
+			// query the single tab checkbox from the rendered tabs list
+			const firstTabCheckboxElement = screen.getByTestId('single_tab_0');
+
+			// test if present in the DOM
+			expect(firstTabCheckboxElement).toBeInTheDocument();
+
+			// test that by default the single tab checkbox is checked
+			expect(firstTabCheckboxElement).toHaveProperty('checked', true);
+
+			// click the single tab checkbox to uncheck it
+			fireEvent.click(firstTabCheckboxElement);
+
+			// test that the single tab checkbox is unchecked
+			expect(firstTabCheckboxElement).toHaveProperty('checked', false);
+
+			// click the single tab checkbox to check it
+			fireEvent.click(firstTabCheckboxElement);
+
+			// test that the single tab checkbox is checked
+			expect(firstTabCheckboxElement).toHaveProperty('checked', true);
+		});
 	});
 })
